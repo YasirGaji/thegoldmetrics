8898ddd129a17a5a7df5444679a82b6b11c060ba
Yasir Gaji <hellopleasantcodes@gmail.com>
2026-02-11 12:09:44 +0100

    fix: for qualit check gate

--- src/app/api/send-welcome/route.ts
+++ src/app/api/send-welcome/route.ts
@@ -2,10 +2,10 @@ import { Resend } from 'resend';
 import WelcomeEmail from '@/emails/welcome';
 import { NextResponse } from 'next/server';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(request: Request) {
   try {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
     const { email, name } = await request.json();
 
     const data = await resend.emails.send({
